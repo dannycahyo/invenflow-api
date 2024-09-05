774a303b1e8c24311bba1b819acc5adbbeb158db
dannycahyo <dannydwicahyono@gmail.com>
2024-09-05 22:05:54 +0700

    chore: Add authMiddleware for token verification

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+export default authMiddleware;
